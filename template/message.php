37ab9f4d3792de4e2f915274f5bdd758faa1bdf5
Jhonatan Carvalho <jhonatancarvalhotorres12@gmail.com>
2022-12-01 18:50:23 -0300

    'teste'

--- template/message.php
+++ template/message.php
@@ -3,14 +3,14 @@
 // session_start();
 if(isset($_SESSION['mensagem'])): ?>	
 
-<script>
-	// Mensagem
-	window.onload = function() {
-		M.toast({html: '<?php echo $_SESSION['mensagem']; ?>'});
-	};
-</script>
+	<script>
+		// Mensagem
+		window.onload = function() {
+			M.toast({html: '<?php echo $_SESSION['mensagem']; ?>'});
+		};
+	</script>
 
 <?php
 endif;
-// session_unset('mensagem');
+unset($_SESSION['mensagem']);
 ?>
