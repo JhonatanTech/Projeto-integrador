e7392e7e2796fb55e3daff96bae8898a12429d51
Jhonatan Carvalho <jhonatancarvalhotorres12@gmail.com>
2022-12-03 11:35:10 -0300

    'teste'

--- view/report.php
+++ view/report.php
@@ -5,10 +5,10 @@ include '../template/menu.html';
 ?>
 
 <div class="container">
-    <iframe title="Relatório Almoxarifado" width="100%" height="600" src="https://app.powerbi.com/reportEmbed?reportId=c0d7c86e-0810-46a0-9c83-35e4efcf3cf9&autoAuth=true&ctid=cf72e2bd-7a2b-4783-bdeb-39d57b07f76f" frameborder="0" allowFullScreen="true"></iframe>
-    
-        <div class="input-field col s12 center">
-        <a id="botao-bi" href="https://app.powerbi.com/reportEmbed?reportId=88621f93-08f1-4e56-a9d1-39843d3a593f&autoAuth=true&ctid=cf72e2bd-7a2b-4783-bdeb-39d57b07f76f" class="waves-effect waves-light btn-large green darken-4" target="_blank">Power BI externo</a>
+    <iframe title="Almoxarifado_befama" width="100%" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=13fece65-8081-4ac5-b259-c38bd4b37758&autoAuth=true&ctid=cf72e2bd-7a2b-4783-bdeb-39d57b07f76f" frameborder="0" allowFullScreen="true"></iframe>
+
+    <div class="input-field col s12 center">
+        <a id="botao-bi" href="https://app.powerbi.com/reportEmbed?reportId=13fece65-8081-4ac5-b259-c38bd4b37758&autoAuth=true&ctid=cf72e2bd-7a2b-4783-bdeb-39d57b07f76f" class="waves-effect waves-light btn-large green darken-4" target="_blank">Power BI externo</a>
     </div>
 </div>
 
